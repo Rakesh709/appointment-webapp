d26da45224362b629b4e594e31b8bc40ade35fd2
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-10-26 13:43:07 +0530

    momgodb and cloudinary connection done

---
+++ backend/config/cloudinary.js
@@ -0,0 +1,12 @@
+import { V2 as cloudinary } from "cloudinary";
+
+const connectCloudinary = async () => {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secrete: process.env.CLOUDINARY_SECRETE_KEY,
+  });
+};
+
+
+export default connectCloudinary;
