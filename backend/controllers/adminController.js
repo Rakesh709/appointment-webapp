313b01717bbe7ea1a162a5a94db68742e0109fd0
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-11-01 23:50:53 +0530

    added middleware

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,18 @@
+
+
+// api for adding Doctor
+
+const addDoctor =  async () => {
+
+    try {
+
+       const {name,email,password,specilaity,degree,exprience,about,fees,address } = req.body;
+       
+
+    } catch (error) {
+        
+    }
+}
+
+
+export {addDoctor}
