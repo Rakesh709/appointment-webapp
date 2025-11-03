0555f736d3f57c0ccada378524f7ddea08f68241
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-11-03 23:35:53 +0530

    update adminController

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -10,7 +10,19 @@ const addDoctor =  async (req,res) => {
        const { name, email, password, speciality, degree, experience, about, fees, address } = req.body;
         const imageFile = req.file;
 
-       console.log({name,email,password,speciality,degree,experience,about,fees,address},imageFile)
+    //    console.log({name,email,password,speciality,degree,experience,about,fees,address},imageFile)
+
+        // add the details in DB...
+        //CHEKING ALL FIELD TO ADD DATA IN DB 
+
+        if(!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address){
+            return res.json({
+                success:false,
+                message:"Missing Details"
+            })
+        }
+
+        
 
         res.status(200).json({
         success: true,
